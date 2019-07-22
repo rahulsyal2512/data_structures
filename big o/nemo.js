63b51c4b04614c34091ede2b8b7fe427a75ba499
Rahul Syal <syal2512@gmail.com>
2019-07-23 00:12:27 +0530

    checking performance

--- big o/nemo.js
+++ big o/nemo.js
@@ -1,11 +1,17 @@
 const nemo = ["nemo"];
+const everyone = ["just", "filling", "array", "with", "some", "items"];
+const large= new Array(1000).fill("nemo");
 
 function findNemo(array) {
+  //try it in browser only
+  let t0 = performance.now();
   for (i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
       console.log("Found nemo");
     }
   }
+  let t1 = performance.now();
+  console.log("It took " + (t1 - t0) + " ms");
 }
 
-findNemo(nemo);
+findNemo(everyone);
