80454e6291756d23165171a9c89d8299cfecec46
Rahul Syal <syal25121997@gmail.com>
2019-07-24 20:08:02 +0530

    added one more example

--- big o/exercise1.js
+++ big o/exercise1.js
@@ -2,7 +2,7 @@ function funChallenge(input) {
   let a = 10; //O(1)
   a = 50 + 3; //O(1)
 
-  for (let i = 0; i < input; i++) { //O(n)
+  for (let i = 0; i < input; i++) {
     anotherFunction(); //O(n)
     let stranger = true; //O(n)
     a++; //O(n)
@@ -12,5 +12,5 @@ function funChallenge(input) {
 
 funChallenge();
 
-// Big O(3+ 4n)
+// Big O(3+ 3n)
 // its just simple calculation ...we will simplify it to O(n)
