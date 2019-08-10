1950cc0b0e64918ef1087c8d34c9a751e12d23dc
Rahul Syal <syal25121997@gmail.com>
2019-08-10 22:40:31 +0530

    different ways for ques solving

--- Questions/compareTwoArrays.js
+++ Questions/compareTwoArrays.js
@@ -11,7 +11,7 @@
 // const Array2 = ["z", "y", "x"];
 // should return true.
 const Array1 = ["x", "y", "z", "i"];
-const Array2 = ["a", "b", "c", "i"];
+const Array2 = ["x", "b", "c", "x"];
 
 // NOT THE BEST SOLUTION
 function containCommonItems(arr1, arr2) {
@@ -25,4 +25,38 @@ function containCommonItems(arr1, arr2) {
   return false;
 }
 
-console.log(containCommonItems(Array1, Array2));
+// O(a*b)
+
+// console.log(containCommonItems(Array1, Array2));
+
+// Another solution
+function containCommonItems2(arr1, arr2) {
+  let obj = { x: true };
+  for (let i = 0; i < arr1.length; i++) {
+    if (!obj[arr1[i]]) {
+      obj[arr1[i]] = true;
+    }
+  }
+  // console.log(obj);
+  for (let j = 0; j < arr2.length; j++) {
+    if (obj[arr2[j]]) {
+      console.log("true");
+    }
+  }
+  // console.log("false");
+}
+
+//O(a+b)
+
+// containCommonItems2(Array1, Array2);
+
+function containCommonItems3(arr1, arr2) {
+  return arr1.some(elem => {
+    return arr2.includes(elem);
+  });
+} 
+
+const check = containCommonItems3(Array1, Array2);
+console.log(check);
+
+//O(n^2)
