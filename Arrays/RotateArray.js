3018bcbfbadb87cc9a366df38c499df984d89f25
Rahul Syal <syal25121997@gmail.com>
2019-09-01 00:36:07 +0530

    rotate array by n elem

--- Arrays/RotateArray.js
+++ Arrays/RotateArray.js
@@ -1,5 +1,11 @@
 var rotate = function(nums, k) {
-  
+  for (let i = 0; i < k; i++) {
+    let temp = nums[nums.length - 1];
+    let tempo = arr[i];
+    arr[i] = arr[temp];
+    arr[temp] = tempo;
+  }
+  console.log(nums);
 };
 
-rotate([1,2,3,4,5,6,7,8], 3)
+rotate([1, 2, 3, 4, 5, 6, 7, 8], 3);
