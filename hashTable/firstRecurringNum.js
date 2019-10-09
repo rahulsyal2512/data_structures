f2c868a7ca68b19c86585af7c88483681803a263
Rahul Syal <syal25121997@gmail.com>
2019-10-10 00:51:53 +0530

    added naive method for example

--- hashTable/firstRecurringNum.js
+++ hashTable/firstRecurringNum.js
@@ -8,5 +8,15 @@
 //Given an array = [2,3,4,5]:
 //It should return undefined
 
+function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+}
 
-// function firstRecurringCharacter(input) 
+firstRecurringCharacter([1, 5, 5, 1, 3, 4, 6]);
