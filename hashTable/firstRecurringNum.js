46e50909aefb845acb0c255f0957aa80873d478a
Rahul Syal <syal25121997@gmail.com>
2019-10-12 00:52:23 +0530

    added faster mehod

--- hashTable/firstRecurringNum.js
+++ hashTable/firstRecurringNum.js
@@ -19,4 +19,18 @@ function firstRecurringCharacter(input) {
   return undefined;
 }
 
-firstRecurringCharacter([1, 5, 5, 1, 3, 4, 6]);
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+  }
+  return undefined;
+}
+
+firstRecurringCharacter2([1, 5, 5, 1, 3, 4, 6]);
+
+// firstRecurringCharacter([1, 5, 5, 1, 3, 4, 6]);
